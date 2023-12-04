f25201486a5ce1515b42e3bc1aaab07aabdcbe38
ydgcdeveloper <ydgcdeveloper@gmail.com>
2023-12-04 13:52:16 -0500

    remove unused code

--- src/app/service/movie/movie.service.ts
+++ src/app/service/movie/movie.service.ts
@@ -1,8 +1,6 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { moviesData } from 'src/app/core/data/movies.data';
-// import { collection, getDocs, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore/lite';
-// import { db } from 'src/app/firebase/firebase';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +9,6 @@ export class MovieService {
   constructor() {}
 
   async getMovies(): Promise<any> {
-    // const moviesCol = collection(db, 'movie');
-    // const moviewSnapshot = await getDocs(moviesCol);
-    // const movieList = moviewSnapshot.docs.map((doc) => {
-    //   return {
-    //     ...doc.data(),
-    //     id: doc.id
-    //   }
-    // });
     return moviesData;
   }
 }
